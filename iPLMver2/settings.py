3093e533292c86960711d414507f866ea8534607
cob-byte <jebarcelona2020@plm.edu.ph>
2025-07-23 03:30:15 +0800

    static

--- iPLMver2/settings.py
+++ iPLMver2/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = []
 static_dir = os.path.join(BASE_DIR, 'CRS', 'static')
